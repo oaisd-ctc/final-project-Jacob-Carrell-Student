f28c28b4ad774e94a7feb99bf1e8ba1cbf18b88a
Jacob-Carrell-Student <jacaa29@student.oaisd.org>
2025-05-01 10:15:29 -0400

    Started making enemy AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float attackCD = 3f;
+    [SerializeField] float attackRange = 1f;
+    [SerializeField] float aggroRange = 4f;
+
+    public GameObject player;
+    public Animator animator;
+    NavMeshAgent agent;
+    float timePassed;
+    float newDestinationCD = 0.5f;
+    public GameObject ragdoll;
+
     public int currentHealth;
     public int maxHealth;
 
@@ -12,9 +24,37 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        animator.SetFloat("speed", agent.velocity.magnitude / agent.speed);
+
+        if (timePassed >= attackCD)
+        {
+            animator.SetTrigger("Attack");
+            timePassed = 0;
+        }
+
+        timePassed += Time.deltaTime;  
+
+        if (newDestinationCD <= 0 && Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+        {
+            newDestinationCD = 0.5f;
+            agent.SetDestination(player.transform.position);
+        }
+
+        newDestinationCD -= Time.deltaTime;
+        transform.LookAt(player.transform);
+    }
+
     public void TakeDamage(int amount)
     {
-        currentHealth -= amount;    
+        currentHealth -= amount;
+        animator.SetTrigger("Damage");
 
         if (currentHealth < 0)
         {
@@ -26,5 +66,7 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+        Instantiate(ragdoll, transform.position, transform.rotation);
+        //ragdoll.AddForce(Impulse);
     }
 }
